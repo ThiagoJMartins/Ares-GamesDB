c7f92a7338b21f17443c9a95646269855a288523
Ares <thiagodev.martins@gmail.com>
2023-12-11 23:22:23 -0300

    Initial Commit

---
+++ api/index.js
@@ -0,0 +1,85 @@
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXNNNWWW0:..........       .                     ....lKl.:0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNk:;;:cldo,..........                                .c' .;kNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx,...........                                       .    ;KWNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0l,..........                                           .cc:kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWX0l'........                                               .cdxKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0olc:;'........                  .                                 .:xKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkl;.........       ......  ..  ... ..       ..                   ..,l0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKc..... ....  ..........','..,,..''. ... .',..                 .c0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0d,... ..';,'',;;''',,'',;;,',;;,',,...'..',,,'...  .             ,kNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkdc. ..,::;,;::;;;;;,,;;;;;;;;,'..','',,,',,,,,''.....             .cKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWXkl;,,;;;;;;;;;;;:;;;;;;;;,'.. ..''',,,,,,,,,,,,'..'....         .,:dXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWW0:,;;;;;;;,;;;;;::cllll,.     ..',,;;;,,'''',,,,,'''...        ,OWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXO:..';:ccloddxxkkO0KK0d,  ..'''',;;;;;::::;'....'',,'''....      ;0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXo.  .:dkkOO0000OOOkdc'  .';::;;::::;;;;;:lolc,.  ....''''''.     :KWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd;..  ..',;:lxkd:'.   ......'coc;;::c::clodooo:.     ...'''..    ,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0cclc;..    .;ol;',,'..',...,lo:,,,:cloodooooolc'.     ...'''.  .lXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNxclc;...,;'.ckxoloolccclccc:::::cloddddoooooolll'    .  ..',,. ,OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXl''''.'cl;ck0kdooooodkOOkkxxxxxxxxxxdooooollllc'   .,;,,,,''..;xXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0loxxddl;ckOkdooooooodxkkkkxxxxxdddoooollllcccc'. .,:c:'..''..,;xNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0dOXKK0xdkkxdooooooooddddxxxdddoooooolllccccccc;',,;,'....;cc;::oXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWklOXXKdlxdoclllooooodddxxdxddolllllllllcccccccc;;:c:;......,c:;;dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0xo;,xKKOo;;:;;,',;:::lddxxxxxdolcc:cccllllcccccccccllc:'..,,,;::;,dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNNX0kxoc;,,,'l00Ox;..',.....',cdxddxxdollcccccclllllllllllccc:'...',',;:::lKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMWNX0xoc:,,,,;;;,,':k0OOd;'',,,,'',:odxdxxdollcccccccllooolllllllll;.......':ccdXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// WMMMMMMMMMMMMMMMMMMMMMWN0xo:;'',,,,,,,,,,,''o0OOOd:,,;;;;:lodddddddollllllccllllooolllllllc,',,'',;,,lONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMWXOdc;,',,,,,,,,,,,,,,,,,ckOkl;:cccc::;;;;:clooolllllllllcclllloooollll:...',,'...kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// WMMMMMMMMMMMMMMMWN0d:,',,,,,,,,,,,,,,,;clox0xxkocdxollllllllcc:::lollllllllllccccccllooooo;.''.''. .lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// xNMMMMMMMMMMMMWNOl;,,,,,,,,,,,,,,;coxOKXWWWWOdkOOx:''',,,,,;:clooolllllllolllccccccccloddl,.,''','.cXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// lKMMMMMMWKkxKNOl,,,,,,,,,,,,,:lxOKNWMMMMMWNOc;d0Od::loollc:::cccccccccccllollcccccccccllc,.','',,''xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// 0WMMMMMNk:;,;c;,,,,,,,,,,,:okKNWMMMMMMMMMNx:,'lOkoldkkxdollllccccccccccllollllcccccccc:,...,'',,,'.dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// WMMMMMW0c;;,,,,,,,,;,,,;lkKWMMMMMMMMMMMWXo,,,';xkddxkxdooooooollccccclooollllcccccc::;,..'','',,,,.lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMW0c;,,,,,,;;;;,:d0NWMMMMMMMMMMMMMNd,,,,,'lxdollllllllllooolllllllllccccc::::;,,''',,,,'',,,,.:XMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMWWXd;,,,,;;;;,:dKWMMMMMMMMMMMMMMMNx;,,,,,'':c:::;;;;:::::cc::::::::::;;;;;;;;;,,,,,,,,,'',,,,.;KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMWOlc;,,,,;;;;,l0WMMMMMMMMMMMMMMMMWKc,,,,,,'..,;;:;;;;;;;;;;;;;;;;;;:;;,,,,;;;;;;,,;,,,,,'',,,'.'OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMWOlc;,',;;;,''dNMMMMMMMMMMMMMMMMMW0:,,,,,,,c;..,:::cc::c::::::;;;,,,;;,,,,;,,,,,;;;;,,,,'',,,'..kMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMWWNk;,;;;,''':0WMMMMMMMMMMMMMMMMW0:,,,;codo:....''..........''',,;;;;;;,,,,,,,,,,;,,,,,,',,,,..lNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMWO:,;;,'',,,xWMMMMMMMMMMMMMMMMWO:,',lkkl;'',''''............'',,,,,,,,,,,,,,,,,,,,,,,,',,','.;dk0KNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMWO:,;,'.',,'lXMMMMMMMMMMMMMMMMWk;'':xkc,'.'cc;'..''''''''',,,,,,;;;;;;;;;;,,,,,,,,,,,,,,,,,'.'ccllkXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMM0:,;'.'',,',kWMMMMMMMMMMMMMMWKl'.:kkc''. .cddc,.'.''''''',,;;;;;;;;;;;;;;;,,,,,,,,,,,,,,,,'..',;clokXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMXo,,'''.',,.,OWMMMMMMMMMMMMMNo',lxOx:''...;lddl;''''''''',,;;;;;;;;;;;;;;;;,,,,,,,,,,,',,,'..''',;:loONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMW0:',,'..','.;kNMMMMMMMMMMMNd';dOOOkl'....,cdxxo:''''''''',,,;;;;;;;;;;;;;,,,,,,,,,,',,,,,'..''''..,:cxXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMWd,,,,'..','.,lONWMMMMMWXkc;ldO0OOOd'....,:cokxo:,'''''''',,,;;;;;;;;;;;,,,,,,,,,,,',,,,,....''''''',:o0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// KNWWWMMMMWO:,,,,,''',,'',:odxkxdooclkkxKK0KKk;....':ccldxd:,''',,''',,;;;;;;;;;;;,,,,,,,,,,'',;;;,............';l0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// :cloddddoo:'',,,,,''','''';:clox0KOO0xkXXKXX0o.....:lc::ldo:,'',,,,'',,;;;;;;;;;,,,,,,,,,,''',;;,,'............':oOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// ::;;;;;,,,'.',,,,,.';ccoxOKXXNNNXK00OkOKXKXXK0l....;llc::clc:;,',,,,,,,;;;;;;;;;;,,,,,,,,,''',;;,''.',;,''......,cclxXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// :::;::;;;,.',,,,;cldk0XNNNNNNNNXXXX0O0K0KKXX000c...'cllc::ccc:;,,,,,,,,,,;;;;;;;;,,,,,,,,,''';;,,''''',;:;,''...':c;,:dO0KXNNWMMMMMMMMMMMMMMMMMMMMMMMM
+// ;;;;;;;,'.',,,:ok0KXNNNNNNNNNNNNNNXKO0XKKKKX00XO;..':lllc::ccc:;;,,;;,,,,;;;;;;;;;,,,,,,,''',;,,,'''''',,;:::;,..':c;',:cldxkO0KKKXWWMMMMMMMMMMMMMMMMM
+// dc;,''...,,;;lxO0XNNNNNNNNNNNNNNNNXK00NX0XXKK0XXx,..,ccccc:::cc::;,,;,,,,,;;;;;:;;,,,,,,,'',;;,,''''''''',,;;:::,',:cc::lx00KXXK00OOO0KNWMMMMMMMMMMMMM
+// kdl:::loodxk000XNNNNNNNNNNNNNNNNNNXK00XNKKXKKKKXXd..,:ccccc::;:cccc;,,,,,,;;;;;:;;,,,,,,,'';;;,,'''''''''''',,;;:::;:cc::lkKKKXNNNNNK0OO00KKKKNWMMMMMM
+// 000KXXXXNNNNKKXNNNNNNNNNNNNNNNNNNXK000KNXXXXKXKKN0;.,:cccccc::;;:cll:;,,,;;;;;:;;;,,,,,''',;;,''''',,,,'',,,''',,,,;,:cccccdkOO0XNNNNNXKOdoxOOOOXWMMMM
+// NNNNNNNNNNNXKXNNNNNNNNNNNNNNNNXKKXKKKKKXNXXXKKXKKXO;.;ccccccc::;;;:llc;,,,;;;;;;;;,,,,'''',:;''''',;;;,,,,,,,,''',''',clc:;;;cok0KNNNNNXXKkdk00dkWMMMM
+// NNNNNNNNNNNXNNXKXNNNNNNNNNNNXXKOKNXXNXKXXXXNXKXNKXXOc,:cccccccc:;,,;:llc,.'',;;;;;;,,'',',::,',,,;;;;;;;;,,,,,,,,,'''':ll:,,,,':x0KXNNNNNXXKkdc;kWMMMW
+// NNNNNNNNNNNNNNXO0XNNNNNNNXKKKK0k0NNXNXKXXXXXNXKXXXXKOl,;ccccccc:;,,,,:lol:,''',,,;;,'','';::,,;;;;;;;;;;;;;;;;;,,,'''';loc;,,,'';d00KXNNNNNXX0o';dxxdx
+// NNNNNNNNNNNNNNX0OXXNNNNNXKK0000kOXNNNXKKXKKKXXKKXNXXX0o,,::::::;;,,,,;:lolc:;'...'''''',,;::,,;;;;;,,,,,,;;;;;;;;;,,,',cooc;,,'',;lk00KXNNNNNXKk:;:cod
+// NNNNNNNNNNNXXKX0OK0KNNNNNK00000OkKNNNNK00KXKXNXKKXNXXXKd;';:oddddooolcc:c:;;;;;,''''',,,,;::;;,,;,''''''',,,''''''''''';ool:;,''',:lxOO0XNNNNNXX0d:loo
+// NNNNNNNNNNNXK0KOkkx0NNNNNX000000O0NNNNNK00XXKNNXXXXXXXNXkc,;o0KKKKK00Okkdoc:;'',;,,,,,,,,;:;;:clllccccc:::;;,'''.......,cool:;,''';codxk0XNNNNXXXKkdO0
+// NNNNNNNNNNXK000OxdxKNNNNNXK000000OKNNNNXK00KXXNNNNXXNXNNX0o,,oO000K0000Okxdol;'''',,,,,,;:cloddddddddddoolc::::::;;;;;;;:loll:,''';:loddx0XXXXXXXKKkdO
+// NNNNNNNNNNXK000OxdkXXNNNNNK00000KO0NNNNNK0OOKKXNNNNXXNNXXX0d:;cdOOOOOOkxxxddoc;,,'',;:cccldddddddddddoooolcc:::::ccccccc:clllc;''',:cloddxOKXXXXXKK0dd
+// NNNNNNNNNNXK0000xdOKXNNNNNX000000K0KNXXXK0OxkKKXNNNNNXNNXKKKOxoccoddxxxxxdoooolc::;:dxdolodddooooooooooollccc:::::::::c:;:ccll:,'.';coooddxOKXXXXKKKOo
+// NNNNNNNNNNX00000xxO0XNNNNNXK00000KKKKXKKK00xdk0KXNXXNNXNXK0KKOkxl:coooodooooollcccc:lddolooooooooooolllllccc:::::::::::;;:cccl:,'.';:odloddxOKXXKKKK0d
+// XNNNNNNNNNX0000OxxOOKNNNNNX00O000KKKKK00000OdokOOKXKKXXXXNXXK0kkkd:cddddddoollllcccccclolclloooooooooolllllcccccccc:::c:;:ccclc;,'.,;lxoldddk0KXXKKK0x
+// 00KKKKXXXXK0OOOOxxkOKNNNNNX00OO000KKK0000000kc:dkO000O0XXKXXK00OOOdclxxxxxxxxddoolcccc:loccllloodddddddddooooooooooooooc;:ccclc;,'.';cddoloodk0KXKK00x
+// OO00O00000OOOOOOkxxOKNNNNNX0OOOOO00000000000Od;;lkOO0OOOKXXXXK0OO0Oxcoxkkkkkkxxxxdolllccllcllodddddddddddddddddddddooooc;:cccll:,'.';:ldolooodk0KK0OOx
+// OOOOOOOOOOOOOOOkxddOXNNNNNX0OOOOOOOO000000000Ol;;cxOkO0OO0KXXKKOOOkxoclxkkkkkkkkkkxddll::cllodddddddddoooooooddooooooooc;:c::loc,''',;clollolldxO0Okkx
+// OOOOOOOOOOOOOOxdlco0NNNNNNK0OOOOkkkxkOO0000000kl;;cdkkO0OO00KKK0OOkdoc:cdkkkkkkkkkkxxol:;:coodddddddooooooooooooooooool:;cc::ldl;,'',;:clolcc:cloxkxxd
+// OOOOOOOOOOOOOxoc::o0XNNNNX00OOkkxddoxkkO0K000OOdlc;:dkkkOOkOO00KK0kxolc:cdxxxxkkkkkxxdoc:;;lddddddddooooooooooooollllll::cc;:loo:,''';:ccloc:;;::coddo
+// OOOOOOOOOOOOkxoc:;cxOKXXK0OOOkkxdoolldxkkO000OOxolc;cdxxkOOkkkkkkkkxxdlcclddxxxxkkxxxdol:;;codddddddoooooooooooooooollc:cc:;cloo:,''',;:cclc;;,;;:llll
+// kOOOOOOOOOOOkdllc;;cokKK0OOOkkxollllclodxkkOOOOkolc;;coddxOOxxkkxxxdoddlcclodxxxxxxxdoolc;;:odddddddooooooooooooooooll::cc:;clll:,,,',;;:cc:;;,,;:cllc
+// xkOOOOOOOOOOkdllc:;::oO0OOkkkxdollllccloodxkkkkkxol;,;:loodkOkkkxdxdooooccc:ldddddddooooc:;:lodddddoollloooooooooooll:;ccc::c:::;,,,',;;:::;;;;,;:cccc
+// odkOOOOOOOOOxdlll:;;:okOOkkkkxdolllllccclldxkkkkxdl:,,,:loooxkkxxdoodooolccccldddddoooool:;:cloooooolllllllllooollll:;:cc:;::;;,,,,,',;;:::;;;;;;:cccc
+// lodkOOOOOOOOxolll:,;:lxkkkkkkxdollllllccccldxxkkkxoc:,,,clooodddooolodooollolcodddooooolc:;::cllolllllllllllllllcc:;,:cc:;;;;,,,;,,,',;;:::;;;;;;;:ccc
+// lloxOOOOOOOkxolll:,,;cdxkkkkkxdllllllllccccodxxkkxdol:;,;cooolddoodooddoooooolcoooooolc::::cc:ccllcccccccllccccccc;;:cc:;;;:;,,,;,,,',;;:::;,;;;;;:::c
+// clldOOOOOOOkdollcc,,;:lxkkkkkkdolllllll::::codxxkkxoll:;,;loooodolodoodoooolllcloollc::::cloolc::ccccccccllllccccc;,:cc;;;;;,,,;;,,'',;;::;,,;;;;;;:::
+// clloxkkOOkxdoollc:,,;;:oxkkkkkxollllllc:;;::codxxkkdool:,,:ooooodlodolodooooll::ccc::::cllloool:::cllllllllllllcc:;:cc:;;;',,,,;,,,',;;;;;,,,;;;;;;;;;
+// clllodxxxdoooolc::;,,;;lxkkkkkxdllllllc:;;;;:lodxkkxdoo:;,,loooodooddllododdocc;;:::cc::::::loc:ccllooooooooollcc;;:c::;;,',;,;;,,,',;;;;;,,,;;;;;;;;;
+
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+	server.listen(3001, () => {
+		console.log("Server listening to port 3001"); // eslint-disable-line no-console
+	});
+});
