125edc20871f0cfc062f8f0732706a716ca3b431
ThiagoJMartins <thiagodev.martins@gmail.com>
2024-01-06 00:20:44 -0300

    DB Update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-	server.listen(3001, () => {
-		console.log("Server listening to port 3001"); // eslint-disable-line no-console
+	server.listen(PORT, () => {
+		console.log(`Server listening to port ${PORT}`); // eslint-disable-line no-console
 	});
 });
--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,11 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/aresgames`,
+	{
+		logging: false, // set to console.log to see the raw SQL queries
+		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+	}
 );
 const basename = path.basename(__filename);
 
@@ -17,32 +17,34 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
-  .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
-  });
+	.filter(
+		(file) =>
+			file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+	)
+	.forEach((file) => {
+		modelDefiners.push(require(path.join(__dirname, "/models", file)));
+	});
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
+	entry[0][0].toUpperCase() + entry[0].slice(1),
+	entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genres } = sequelize.models;
+Videogame.belongsToMany(Genres, { through: "videogame_genre" });
+Genres.belongsToMany(Videogame, { through: "videogame_genre" });
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+	...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+	conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
