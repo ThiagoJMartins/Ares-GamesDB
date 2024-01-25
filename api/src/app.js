82515cb11dd6342d22eacf315b7461680be6e886
Ares <thiagodev.martins@gmail.com>
2024-01-25 16:44:12 -0300

    lets see

--- api/src/app.js
+++ api/src/app.js
@@ -24,16 +24,16 @@ server.use(
 		},
 	})
 );
-// server.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
-// 	res.header("Access-Control-Allow-Credentials", "true");
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-// 	next();
-// });
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
 
 server.use("/videogames", videogames);
 server.use("/genres", genres);
